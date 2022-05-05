617f7c4cc5a89e366508239c8d06f3cd57e3328d
KYC127 <ibiu1222@gmail.com>
2022-05-06 04:00:54 +0800

    Update Inference.py

--- Inference.py
+++ Inference.py
@@ -132,7 +132,7 @@ def inference(input_data):
     prediction[prediction>threshold] = 1
     prediction[prediction<=threshold] = 0
     print (f'prediction:{prediction}')
-    print (f'real label:{test_y[r:r+1][0]}')
+    #print (f'real label:{test_y[r:r+1][0]}')
     print (f'invoke time:{invoke_time}s')
 
     #plt_x = np.reshape(input_data,(4096,))
